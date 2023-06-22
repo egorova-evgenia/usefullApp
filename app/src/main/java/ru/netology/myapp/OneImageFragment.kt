263e68d06642e41c39b9f82d21f5b0298716c2fc
egorova-evgenia <radouga.jenny@gmail.com>
2023-06-22 21:43:55 +0300

    OK commit

--- app/src/main/java/ru/netology/myapp/OneImageFragment.kt
+++ app/src/main/java/ru/netology/myapp/OneImageFragment.kt
@@ -34,10 +34,10 @@ class OneImageFragment : Fragment() {
                 val post = it.posts.find { it.id == postId } ?: return@observe
 //                post.attachment?.
                 val BASE_URL ="http://10.0.2.2:9999"
-                val url="$BASE_URL/images/${post.attachment?.url}"
+                val url="$BASE_URL/media/${post.attachment?.url}"
                 Glide.with(binding.oneImage)
                     .load(url)
-                    .error(R.drawable.baseline_font_download_off_24)
+//                    .error(R.drawable.baseline_font_download_off_24)
                     .timeout(10_000)
                     .into(binding.oneImage)
             }
